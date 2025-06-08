d17ab8694fd0e1e689fd39b413f55fcbf9c63c72
Bianca200303 <biancafseminario@gmail.com>
2025-06-08 03:30:04 -0500

    añadiendo marcas en sql

--- app/db/categorias.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE categorias (
-    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL
-);
-
-INSERT INTO categorias (nombre) VALUES
-('Lácteos'), ('Granos'), ('Aceites'), ('Limpieza'), ('Higiene'),
-('Snacks'), ('Bebidas'), ('Conservas'), ('Salsas');
